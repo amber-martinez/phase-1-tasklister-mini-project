f6e6c16a59ebee5cf9affd329328e6791e56f9cd
Amber <martinez.amber1028@gmail.com>
2022-01-09 16:46:43 -0800

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+document.addEventListener('DOMContentLoaded', () => {
+  let form = document.getElementById('create-task-form')
+  form.addEventListener('submit', (e) => {
+    e.preventDefault()
+    addToDo(e.target.new_task_description.value)
+    form.reset()
+  })
+})
+
+function addToDo(thingToDo){
+  let p = document.createElement('li')
+  let btn = document.createElement('button')
+  // let priorityTag = document.getElementById('priority')
+  btn.addEventListener('click', deleteThing)
+  p.textContent = `${thingToDo} `
+  btn.textContent = 'delete'
+  p.appendChild(btn)
+  // p.appendChild(priorityTag)
+  document.getElementById('list').appendChild(p)
+  
+}
+
+function deleteThing(e) {
+  e.target.parentNode.remove()
+}
